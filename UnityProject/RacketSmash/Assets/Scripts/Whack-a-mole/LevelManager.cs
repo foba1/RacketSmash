0ccfe98f0097974f7f89d6a96b06d2b5b4fcd724
Hyojung Lee <leeexpert@cau.ac.kr>
2022-11-14 13:02:52 +0900

    두더지 움직임 구현 해야함

--- UnityProject/RacketSmash/Assets/Scripts/Whack-a-mole/LevelManager.cs
+++ UnityProject/RacketSmash/Assets/Scripts/Whack-a-mole/LevelManager.cs
@@ -28,12 +28,20 @@ namespace WhackAMole
         // Update is called once per frame
         void Update()
         {
+            // Elapsed time
             elapsedTime += Time.deltaTime;
             if (elapsedTime >= timeOut)
             {
                 gameOverText.SetActive(true);
             }
             timer.text = "Elapsed Time : " + string.Format("{0:N2}", elapsedTime.ToString());
+
+            // 게임 종료 테스트
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                gameOverText.GetComponent<TextMeshPro>().text = "Game Over !\nScore : " + totalScore.ToString();
+                gameOverText.SetActive(true);
+            }
         }
 
         
